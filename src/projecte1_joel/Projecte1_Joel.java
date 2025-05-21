9aec8d1f1639ef1edfcb18aa9275fb156b752e7d
Jowy05 <adam23jdelolmo@inslaferreria.cat>
2025-05-21 20:16:25 +0200

    Adeu Afegit

---
+++ src/projecte1_joel/Projecte1_Joel.java
@@ -0,0 +1,11 @@
+package projecte1_joel;
+
+public class Projecte1_Joel {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hola");
+        System.out.println("Adeu");
+    }
+    
+}

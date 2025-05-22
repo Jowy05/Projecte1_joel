7489f60d0a89dc43f5c2df217866ee086c89c54f
leoFerreria <adam23ldominguez@inslaferreria.cat>
2025-05-22 17:43:48 +0200

    sout Leomar

--- src/projecte1_joel/Projecte1_Joel.java
+++ src/projecte1_joel/Projecte1_Joel.java
@@ -5,6 +5,8 @@ public class Projecte1_Joel {
     public static void main(String[] args) {
         
         System.out.println("Hola");
+        System.out.println("Leomar");
+        System.out.println("Leomar");
     }
     
 }

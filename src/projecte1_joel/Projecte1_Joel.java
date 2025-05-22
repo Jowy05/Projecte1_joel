378cdbf7d81c6dee2ef504e86c00288532e94f75
Jowy05 <adam23jdelolmo@inslaferreria.cat>
2025-05-22 17:34:05 +0200

    sout joel

--- src/projecte1_joel/Projecte1_Joel.java
+++ src/projecte1_joel/Projecte1_Joel.java
@@ -5,6 +5,7 @@ public class Projecte1_Joel {
     public static void main(String[] args) {
         
         System.out.println("Hola");
+        System.out.println("Joel Del Olmo");
     }
     
 }
